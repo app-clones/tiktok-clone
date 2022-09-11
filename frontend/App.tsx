121acaf64d23633fff9ca627090b1bf52097b38b
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2022-09-10 19:18:20 -0700

    :ambulance: Remove Firebase Config

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -3,15 +3,9 @@ import { StyleSheet, Text, View } from "react-native";
 
 import firebase from "firebase/app";
 
-firebase.initializeApp({
-    apiKey: "AIzaSyA9BP3wGDJEHWIJ4f5WZYahKNftd0RDaBI",
-    authDomain: "tiktok-clone-b29c0.firebaseapp.com",
-    databaseURL: "https://tiktok-clone-b29c0-default-rtdb.firebaseio.com",
-    projectId: "tiktok-clone-b29c0",
-    storageBucket: "tiktok-clone-b29c0.appspot.com",
-    messagingSenderId: "834280486754",
-    appId: "1:834280486754:web:44cb3ce8860cd4a7855e3b"
-});
+import firebaseConfig from "./firebaseConfig";
+
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
     return (
